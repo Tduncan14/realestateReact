6fa05d79ae83f395fdabd0593fa409b6b82da032
tduncan14 <nbatre1@yahoo.com>
2023-08-20 03:16:38 -0700

    keep moving forward

--- reactestate/src/App.js
+++ reactestate/src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
- 
+    <Router>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ reactestate/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+
+ 
+
+
+const Home = () =>  {
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+               This is home
+        </div>
+    )
+}
+
+
+export default Home
